8e7a4dcbd091c02db547d90ca48e74c540e2bcb1
Julianne <julianne.leblanc-peltier1@stu.ocsb.ca>
2025-03-05 19:06:59 +0000

    added area program in python

---
+++ area.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Created by: Julianne Leblanc-Peltier
+Created on: March 2025
+This module calculates the area of a circle
+    with radius 15 mm.
+"""
+
+import math
+
+def main() -> None:
+    """
+    This function calculates the circumference of a circle with a diameter of 42 mm
+    """
+
+    print("If a circle has a diameter of 42 mm\n")
+    print(f"Circumference is: {(math.pi * 42):.5f}")
+
+    print("\nDone! :)")
+
+
+if __name__ == "__main__":
+    main()
